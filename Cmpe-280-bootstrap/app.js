805bd47c58aaec518b40f527071c79d0952ae46d
kumar Vikramaditya <vikramaditya.in@gmail.com>
2015-10-25 17:52:04 -0700

    First Commit

---
+++ Cmpe-280-bootstrap/app.js
@@ -0,0 +1,83 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var landing = require('./routes/landing');
+
+var mobile = require('./routes/mobile');
+var bigdata = require('./routes/bigdata');
+var iot = require('./routes/iot');
+
+var code = require('./routes/code');
+var contacts = require('./routes/contacts');
+var locations = require('./routes/locations');
+var management = require('./routes/management');
+var process = require('./routes/process');
+
+var blog = require('./routes/blog');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', landing);
+
+app.use('/mobile', mobile);
+app.use('/bigdata', bigdata);
+app.use('/iot', iot);
+
+app.use('/code', code);
+app.use('/contacts', contacts);
+app.use('/locations', locations);
+app.use('/management', management);
+app.use('/process', process);
+
+
+app.use('/blog', blog);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
